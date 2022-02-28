56a177a13e7ed0ca9be963739cdd38189dcff010
lcmrl <lmorelli@fbk.eu>
2022-02-28 10:00:00 +0100

    Not relevant modification to the config file.

--- Tile_matcher/config.py
+++ Tile_matcher/config.py
@@ -1,7 +1,7 @@
-image_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\lfnet_nadir\imgs_folder"
-desc_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\lfnet_nadir\desc"
-colmap_desc_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\lfnet_nadir\colmap_desc"
-matches_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\lfnet_nadir\matches"
-tile_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\lfnet_nadir\tiles_folder"
+image_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\aerial_trento\Trento_resized_JPG"
+desc_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\aerial_trento\lfnet\lfnet_descriptor"
+colmap_desc_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\aerial_trento\lfnet\colmap_desc"
+matches_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\aerial_trento\lfnet\colmap_matches"
+tile_folder = r"C:\Users\User\Desktop\Luca\Projects\Aerial-images\aerial_trento\aerial_trento_tiles"
 cross_check = True
 pool_N = 4                                          # nuber of logical processors to be used
